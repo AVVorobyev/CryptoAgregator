ad6155fa6df2281b1d2c0469749cfad066004e85
AVVorobyev <89789085+AVVorobyev@users.noreply.github.com>
2023-05-21 20:57:53 +0700

    Create SymbolPrice.cs

---
+++ CryptoAgregator.Core/Data/SymbolPrice.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoAgregator.Core.Data
+{
+    public class SymbolPrice : Symbol
+    {
+        public decimal Price { get; protected set; }
+
+        public SymbolPrice(string name, decimal price) : base(name)
+        {
+            Price = price;
+        }
+    }
+}
