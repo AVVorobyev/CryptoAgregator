a3c2003c68eeac31252fa92b9404ca18c25b4c95
AVVorobyev <89789085+AVVorobyev@users.noreply.github.com>
2023-05-21 20:58:06 +0700

    Create Agregator.cs

---
+++ CryptoAgregator.Core/Data/Agregator.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptoAgregator.Core.Data
+{
+    public abstract class Agregator
+    {
+        public abstract Task<Result<IEnumerable<Symbol>>> GetSymbolsAsync();
+        public abstract Task<Result<SymbolPrice>> GetCurrentPriceAsync(string symbol);
+    }
+}
