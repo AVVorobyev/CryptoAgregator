b7a3267e39d117e9a39f640124f040e733d92cc4
AVVorobyev <89789085+AVVorobyev@users.noreply.github.com>
2023-05-21 20:57:45 +0700

    Create Symbol.cs

---
+++ CryptoAgregator.Core/Data/Symbol.cs
@@ -0,0 +1,18 @@
+﻿namespace CryptoAgregator.Core.Data
+{
+    public class Symbol
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            protected set { _name = value.ToUpper(); }
+        }
+
+        public Symbol(string name)
+        {
+            _name = name;
+        }
+    }
+}
